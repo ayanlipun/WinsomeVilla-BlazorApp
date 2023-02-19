33e61cf77e8714a2f3af391bf25135804364f0e5
Ayan <ayanansuman.sahoo@gmail.com>
2023-02-19 19:05:07 +0530

    Add project files.

---
+++ WinsomeVilla_Server/Model/BlazorAmenities.cs
@@ -0,0 +1,10 @@
+﻿namespace WisnomeVilla_Server.Model
+{
+    public class BlazorAmenities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+
+    }
+}
---
+++ WinsomeVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,12 @@
+﻿namespace WisnomeVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public bool IsActive { get; set; }
+        public int Price  { get; set; }
+
+        public List<BlazorRoomProp> BlazorRoomProps { get; set; }
+    }
+}
